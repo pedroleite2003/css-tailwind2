a48ceb91a52386edc2ad67d11e8e05f4d9f216f4
unknown <pedropinho0680@digitalcollege.internal>
2024-12-06 09:38:51 -0300

    mover

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {
+        colors: {
+            amarelo:'#E6C744',
+            cinza:'#C2C8DA',
+        }
+      },
+    },
+    plugins: [],
+  }
