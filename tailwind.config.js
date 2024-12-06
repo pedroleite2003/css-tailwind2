ca6d8740a4e09d61b2ef9c937227a54f9af186a3
unknown <pedropinho0680@digitalcollege.internal>
2024-12-06 12:01:59 -0300

    continuação

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ module.exports = {
         colors: {
             amarelo:'#E6C744',
             cinza:'#C2C8DA',
+        },
+        screens: {
+            '3xl':'1920px'
         }
       },
     },
